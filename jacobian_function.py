e3904eb22dda7234574e6f0f5a22cf5c21e9560c
eliroc98 <elisabetta.rocchetti0@gmail.com>
2024-04-16 12:41:08 +0200

    pullback experiments

--- jacobian_function.py
+++ jacobian_function.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def jacobian(output_, input_):
     """
     Explicitly compute the full Jacobian matrix.
@@ -9,12 +10,33 @@ def jacobian(output_, input_):
         input (torch.Tensor): A model input with gradient attached
 
     Returns:
-    
+
     torch.Tensor: The Jacobian matrix, of dimensions torch.Size([len(output), len(input)])
     """
 
-    du = torch.stack([torch.autograd.grad([output_[i]], input_, retain_graph=True)[0] for i in range(output_.size(0))], dim=-1)
+    du = torch.stack(
+        [
+            torch.autograd.grad([output_[i]], input_, retain_graph=True)[0]
+            for i in range(output_.size(0))
+        ],
+        dim=-1,
+    )
     J = du[0]
 
-    #Return a new Tensor detached from the current graph.
+    # Return a new Tensor detached from the current graph.
     return J.detach()
+
+
+def jacobian2D(output_, input_):
+    grad = []
+    for h in range(output_.size(0)):
+        for w in range(output_.size(1)):
+            grad.append(
+                torch.autograd.grad(
+                    outputs=output_[h, w], inputs=input_, retain_graph=True
+                )[0]
+            )
+    grad = torch.stack(grad, dim=-1).squeeze()
+    # no batch
+    grad = grad.reshape(*tuple(output_.size()), *tuple(output_.size())).detach()
+    return grad
--- simec_vit_feature_importance.py
+++ simec_vit_feature_importance.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from matplotlib.patches import Rectangle
-from jacobian_function import jacobian
+from jacobian_function import jacobian, jacobian2D
 from data import prepare_data
 from vit import ViTForClassfication
 
@@ -33,6 +33,43 @@ CONFIG = {
     "num_channels": 1,
     "qkv_bias": True,
 }
+
+
+class PatchDecoder(nn.Module):
+    """
+    Convert the embeddings so that they can be displayed as images
+    """
+
+    def __init__(
+        self, image_size, patch_size, num_channels, hidden_size, positional_embeddings
+    ):
+        super().__init__()
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.num_channels = num_channels
+        self.hidden_size = hidden_size
+        self.positional_embeddings = positional_embeddings
+        # Calculate the number of patches from the image size and patch size
+        self.num_patches = self.image_size // self.patch_size
+        # Create a projection layer to convert embeddings into images
+        self.projection = nn.ConvTranspose2d(
+            self.hidden_size,
+            self.num_channels,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+        )
+
+    def forward(self, x):
+        # reverting the operations done by PatchEmbedder
+        x = x - self.positional_embeddings
+        # removing first embedding because it is the classifier's
+        x = (
+            x[:, 1:, :]
+            .transpose(1, 2)
+            .reshape(1, self.hidden_size, self.num_patches, self.num_patches)
+        )
+        x = self.projection(x)
+        return x
 
 
 def load_model(fname, device):
@@ -68,33 +105,92 @@ def simec_vit(
     print_every_n_iter=100,
     img_out_dir=".",
 ):
+    def simec(input_simec, output_simec, select_patch=True):
+        # Compute the pullback metric
+        if select_patch:
+            jac = jacobian(output_simec, input_simec)[eq_class_patch_id]
+            jac_t = torch.transpose(jac, -2, -1)
+            tmp = torch.mm(jac, g)
+            if device.type == "mps":
+                # mps doen't support float64, must convert in float32
+                pullback_metric = torch.mm(tmp, jac_t).type(torch.float32)
+            else:
+                # The conversion to double is done in order to avoid the following error:
+                # The algorithm failed to converge because the input matrix is ill-conditioned or has too many repeated eigenvalues
+                pullback_metric = torch.mm(tmp, jac_t).type(torch.double)
+            return torch.linalg.eigh(pullback_metric, UPLO="U")
+        else:
+            jac = jacobian2D(output_simec, input_simec)
+            jac_t = jac.permute(-1, -2, -3, -4)
+            eigenvalues, eigenvectors = [], []
+            for h in range(jac.size(0)):
+                for w in range(jac.size(1)):
+                    tmp = torch.mm(jac[h, w], g)
+                    if device.type == "mps":
+                        pullback_metric = torch.mm(tmp, jac_t[:, :, w, h]).type(
+                            torch.float32
+                        )
+                        eigenvalues.append()
+                    else:
+                        pullback_metric = torch.mm(tmp, jac_t[:, :, w, h]).type(
+                            torch.double
+                        )
+                    # Compute eigenvalues and eigenvectors
+                    values, vectors = torch.linalg.eigh(pullback_metric, UPLO="U")
+                    eigenvalues.append(values)
+                    eigenvectors.append(vectors)
+            eigenvectors = torch.stack(eigenvectors, dim=-1).reshape(28, 28, 28, 28)
+            eigenvalues = torch.stack(eigenvalues, dim=-1).reshape(28, 28, 28)
+            return eigenvectors, eigenvalues
 
-    fname = img_out_dir + "start.png"
+    # fname = img_out_dir + "start.png"
     image = starting_img.squeeze().cpu().detach().numpy()
     _, ax = plt.subplots()
     ax.imshow(image, cmap="gray")
-    plt.savefig(fname)
-    plt.clf()
+    # plt.savefig(fname)
+    # plt.clf()
 
-    back_projection = nn.ConvTranspose2d(48, 1, 2, 2)
+    # define decoder to plot images from embeddings
+    decoder = PatchDecoder(
+        image_size=model.image_size,
+        patch_size=patch_size,
+        num_channels=starting_img.shape[1],
+        hidden_size=model.hidden_size,
+        positional_embeddings=model.embedding.position_embeddings,
+    )
+
+    # prepare eigenvectors to project embeddings back to the image space
+    output_decoder = decoder(model.embedding(starting_img.requires_grad_(True)))
+    g = torch.eye(output_decoder.shape[-1])  # , output_emb.shape[-2] - 1)
+    eigenvectors_decoder, _ = simec(
+        input_simec=starting_img,
+        output_simec=output_decoder.squeeze(),
+        select_patch=False,
+    )
+
     # Clone and require gradient of the embedded input and prepare for the first iteration
     emb_inp_simec = model.embedding(starting_img).requires_grad_(True)
-
-    fname = img_out_dir + ".png"
-    back_emb_inp_simec = emb_inp_simec.clone() - model.embedding.position_embeddings
-    back_emb_inp_simec = (
-        back_emb_inp_simec[:, 1:, :].transpose(1, 2).reshape(1, 48, 14, 14)
-    )
-    back_emb_inp_simec = back_projection(back_emb_inp_simec)
-    image = back_emb_inp_simec.squeeze().cpu().detach().numpy()
-    _, ax = plt.subplots()
-    ax.imshow(image, cmap="gray")
-    plt.savefig(fname)
-    plt.clf()
 
     # Build the identity matrix that we use as standard Riemannain metric of the output embedding space.
     embedding_dimension = emb_inp_simec.shape[-1]
     g = torch.eye(embedding_dimension)
+
+    # Plot embeddings as image with decoder
+    image_emb_inp_simec = decoder(emb_inp_simec.clone()).squeeze().type(torch.double)
+    basis_changes = []
+    for h in range(image_emb_inp_simec.size(0)):
+        for w in range(image_emb_inp_simec.size(1)):
+            eigevec = eigenvectors_decoder[h, w]
+            eigevec_inverse = torch.inverse(eigevec)
+            basis_change = torch.mm(eigevec_inverse, image_emb_inp_simec)
+            basis_change = torch.mm(basis_change, eigevec)
+            basis_changes.append(basis_change)
+    # fname = img_out_dir + ".png"
+    image = basis_change.squeeze().cpu().detach().numpy()
+    _, ax = plt.subplots()
+    ax.imshow(image, cmap="gray")
+    # plt.savefig(fname)
+    # plt.clf()
 
     # Compute the output of the encoder. This is the output which we want to keep constant
     encoder_output = model.encoder(emb_inp_simec)[0]
@@ -102,21 +198,12 @@ def simec_vit(
     # Keep track of the length of the polygonal
     distance = 0.0
     for i in range(n_iterations):
+        # simec --------------------------------------------------------------
+        eigenvalues, eigenvectors = simec(
+            output_simec=encoder_output[0, 0], input_simec=emb_inp_simec
+        )
 
-        # Compute the pullback metric
-        jac = jacobian(encoder_output[0, 0], emb_inp_simec)[eq_class_patch_id]
-        jac_t = torch.transpose(jac, 0, 1)
-        tmp = torch.mm(jac, g)
-        if device.type == "mps":
-            # mps doen't support float64, must convert in float32
-            pullback_metric = torch.mm(tmp, jac_t).type(torch.float32)
-        else:
-            # The conversion to double is done in order to avoid the following error:
-            # The algorithm failed to converge because the input matrix is ill-conditioned or has too many repeated eigenvalues
-            pullback_metric = torch.mm(tmp, jac_t).type(torch.double)
-
-        # Compute eigenvalues and eigenvectors
-        eigenvalues, eigenvectors = torch.linalg.eigh(pullback_metric, UPLO="U")
+        # random walk step ---------------------------------------------------
 
         # Select a random eigenvectors corresponding to a null eigenvalue.
         # We consider an eigenvalue null if it is below a threshold value.
@@ -124,14 +211,12 @@ def simec_vit(
         number_null_eigenvalues = torch.count_nonzero(zero_eigenvalues)
         id_eigen = torch.randint(0, number_null_eigenvalues, (1,)).item()
         null_vec = eigenvectors[:, id_eigen].type(torch.float)
-
         # Proceeed along a null direction
         emb_inp_simec = emb_inp_simec.detach()
         emb_inp_simec[0, eq_class_patch_id] = (
             emb_inp_simec[0, eq_class_patch_id] + null_vec * delta
         )
         distance += eigenvalues[id_eigen].item() * delta
-
         # Clamp the tensor in [-1,1].
         clamp_lower = emb_inp_simec < -1.0
         emb_inp_simec[clamp_lower] = -1.0
@@ -150,8 +235,8 @@ def simec_vit(
 
             image = emb_inp_simec - model.embedding.position_embeddings
             image = image[:, 1:, :].transpose(1, 2).reshape(1, 48, 14, 14)
-            image = back_projection(image)
-            image = starting_img + back_emb_inp_simec - image
+            image = decoder(image)
+            image = starting_img + image_emb_inp_simec - image
             image = image.squeeze().cpu().detach().numpy()
             _, ax = plt.subplots()
             ax.imshow(image, cmap="gray")
