3a150b1718cb9282bfdc6637bae22a3f7e1847a2
eliroc98 <elisabetta.rocchetti0@gmail.com>
2024-04-18 17:54:42 +0200

    removed jacobian2D

--- jacobian_function.py
+++ jacobian_function.py
@@ -25,18 +25,3 @@ def jacobian(output_, input_):
 
     # Return a new Tensor detached from the current graph.
     return J.detach()
-
-
-def jacobian2D(output_, input_):
-    grad = []
-    for h in range(output_.size(0)):
-        for w in range(output_.size(1)):
-            grad.append(
-                torch.autograd.grad(
-                    outputs=output_[h, w], inputs=input_, retain_graph=True
-                )[0]
-            )
-    grad = torch.stack(grad, dim=-1).squeeze()
-    # no batch
-    grad = grad.reshape(*tuple(output_.size()), *tuple(output_.size())).detach()
-    return grad
