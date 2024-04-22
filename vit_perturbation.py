847074443b25f15e8ab4bf317770a513b7b38a01
Davide Riva <60431521+dave-shore@users.noreply.github.com>
2024-04-22 17:10:14 +0200

    vit perturbation

--- vit_perturbation.py
+++ vit_perturbation.py
@@ -1,12 +1,20 @@
 from utils import *
 import argparse
+import torch
+import numpy as np
+from time import time
+import os
 
 
 def parse_args():
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", type=str, required=True)
     parser.add_argument("--config-path", type=str, required=True)
     parser.add_argument("--device", type=str)
+    parser.add_argument("--timer", type = float, required = True)
+    parser.add_argument("--pert-step", type = float, required = True)
+    parser.add_argument("--out-dir", type = str, required =True)
 
     args = parser.parse_args()
     if args.device is None:
@@ -19,12 +27,48 @@ def parse_args():
     return args
 
 
+def orthogonal_vector(v):
+
+    M = torch.eye(len(v))
+    random_dim = torch.randint(high = len(v))
+    M[random_dim,:] = -1*v / v[random_dim]
+    M[random_dim, random_dim] = 0
+
+    return M @ v
+
+
+def perturbation(model, starting_img, step, direction, y):
+
+    noise = direction.reshape(starting_img.shape)
+
+    new_img = starting_img + step*noise
+
+    logits = model(new_img).flatten().numpy()
+    new_y = np.argmax(logits)
+
+    orthogonal_direction = 0.1*orthogonal_vector(direction)
+    new_direction = direction + orthogonal_direction
+
+    while new_y != y:
+
+        noise = new_direction.reshape(starting_img.shape)
+        new_img = starting_img + step*noise
+
+        logits = model(new_img).flatten().numpy()
+        new_y = np.argmax(logits)
+
+    return new_img, new_direction
+
+
 def main():
 
     args = parse_args()
     model_path = args.model_path
     config_path = args.config_path
     device = torch.device(args.device)
+    initial_time = args.time
+    step = args.pert_step
+    img_out_dir = args.out_dir
 
     # MNIST data
     trainloader, _ = prepare_data(test=False)
@@ -42,3 +86,45 @@ def main():
     imgs = imgs.to(device)
     # take first image keeping batch dimension
     img = imgs[0].unsqueeze(0)
+
+    # Initialize mean and standard deviation of noise to 0 and 1
+    mu = 0
+    sd = 1
+    direction = torch.normal(mean = mu, std = sd, size = img.shape).flatten()
+    count = 0
+
+    timer = initial_time
+
+    while timer > 0:
+
+        tic = time()
+
+        logits = model(img).flatten().numpy()
+        y = np.argmax(logits)
+
+        img, direction = perturbation(model = model,
+                     starting_img = img,
+                     step = step,
+                     direction = direction,
+                     y = y)
+        
+        fname = os.path.join(
+                    img_out_dir, str(count) + ".png"
+                )
+        
+        count += 1
+        timedelta = time() - tic
+        timer -= timedelta
+
+    stats = {
+        "time": int(initial_time - timer),
+        "count": count
+    }
+
+    with open(os.path.join(img_out_dir, "stats.json"), "w") as f:
+        json.dump(stats, f)
+
+
+
+if __name__ == "__main__":
+    main()
