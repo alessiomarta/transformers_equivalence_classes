ff6b8b4c4650801a116d6d274135069c23b8c2f8
Davide Riva <60431521+dave-shore@users.noreply.github.com>
2024-04-29 14:25:25 +0200

    limit iterations

--- analysis/vit_perturbation.py
+++ analysis/vit_perturbation.py
@@ -34,34 +34,47 @@ def parse_args():
 def orthogonal_vector(v):
 
     M = torch.eye(len(v))
-    random_dim = torch.randint(high=len(v))
+    random_dim = np.random.randint(low = 0, high=len(v))
     M[random_dim, :] = -1 * v / v[random_dim]
     M[random_dim, random_dim] = 0
 
     return M @ v
 
 
-def perturbation(model, starting_img, step, direction, y):
+def perturbation(model, starting_img, step, direction, y, device):
 
     noise = direction.reshape(starting_img.shape)
 
     new_img = starting_img + step * noise
 
-    logits = model(new_img).flatten().numpy()
+    logits = model(new_img.unsqueeze(0))[0].flatten().cpu().detach().numpy()
     new_y = np.argmax(logits)
 
-    orthogonal_direction = 0.1 * orthogonal_vector(direction)
-    new_direction = direction + orthogonal_direction
+    orthogonal_direction = 0.1 * orthogonal_vector(direction.cpu())
+    new_direction = direction.cpu() + orthogonal_direction
+    new_direction = new_direction.to(device)
 
-    while new_y != y:
+    iter = 0
+
+    while new_y != y and iter < 100:
 
         noise = new_direction.reshape(starting_img.shape)
         new_img = starting_img + step * noise
 
-        logits = model(new_img).flatten().numpy()
+        logits = model(new_img.unsqueeze(0))[0].flatten().cpu().detach().numpy()
         new_y = np.argmax(logits)
 
-    return new_img, new_direction
+        orthogonal_direction = 0.1 * orthogonal_vector(direction.cpu())
+        new_direction = -1*direction.cpu() + orthogonal_direction
+        new_direction = new_direction.to(device)
+
+        iter += 1
+
+    print("Generated 1 image")
+
+    generated = int(iter < 100)
+
+    return new_img, new_direction, generated
 
 
 def main():
@@ -84,15 +97,19 @@ def main():
         config_path=config_path,
         device=device,
     )
+    model = model.to(device)
 
     deactivate_dropout_layers(model)
 
     
     for name, img in zip(names, images):
+
+        print("Image:", name)
+
         # Initialize mean and standard deviation of noise to 0 and 1
         mu = 0
         sd = 1
-        direction = torch.normal(mean=mu, std=sd, size=img.shape).flatten()
+        direction = torch.normal(mean=mu, std=sd, size=img.shape).flatten().to(device)
         count = 0
         norm = Normalize(vmin=-1, vmax=1)
 
@@ -100,13 +117,15 @@ def main():
 
         while timer > 0:
 
+            print("Timer:", timer)
+
             tic = time()
 
-            logits = model(img).flatten().numpy()
+            logits = model(img.unsqueeze(0))[0].flatten().cpu().detach().numpy()
             y = np.argmax(logits)
 
-            img, direction = perturbation(
-                model=model, starting_img=img, step=step, direction=direction, y=y
+            img, direction, generated = perturbation(
+                model=model, starting_img=img, step=step, direction=direction, y=y, device = device
             )
 
             timedelta = time() - tic
@@ -120,7 +139,7 @@ def main():
             plt.savefig(fname)
             plt.close()
 
-            count += 1
+            count += generated
 
         stats = {"time": int(initial_time - timer), "count": count}
 
