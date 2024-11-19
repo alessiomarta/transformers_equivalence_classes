cb2d1cca91724194425b8e11b637c97f44237f0e
eliroc98 <elisabetta.rocchetti0@gmail.com>
2024-11-19 09:26:56 +0100

    trying to fix white borders

--- data-preprocessing/image_convert.py
+++ data-preprocessing/image_convert.py
@@ -9,10 +9,12 @@ import matplotlib.pyplot as plt
 import argparse
 import gc
 import sys
+
 sys.path.append("./experiments")
 sys.path.append("./analysis")
 from experiments.experiments_utils import *
 from analysis.vit_attention_exp import *
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -20,7 +22,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--model-path", type=str, required=True)
     parser.add_argument("--config-path", type=str, required=True)
     parser.add_argument("--device", type=str)
-    parser.add_argument("--sample", type=int, default = 200)
+    parser.add_argument("--sample", type=int, default=200)
 
     args = parser.parse_args()
     if args.device is None:
@@ -32,9 +34,7 @@ if __name__ == "__main__":
 
     args = parse_args()
 
-    transform = transforms.Compose(
-        [transforms.ToTensor()]
-    )
+    transform = transforms.Compose([transforms.ToTensor()])
 
     test_sample_size = args.sample
     data_dir = f"./{args.dataset.lower()}_data"
@@ -50,15 +50,17 @@ if __name__ == "__main__":
     )
 
     testset = getattr(datasets, args.dataset.upper())(
-        root=data_dir,
-        download=True,
-        train=False,
-        transform = transform
+        root=data_dir, download=True, train=False, transform=transform
     )
 
     if test_sample_size is not None:
         # Randomly sample a subset of the test set
-        _, indices = train_test_split(list(range(len(testset))), test_size = test_sample_size, random_state=42, stratify=testset.targets)
+        _, indices = train_test_split(
+            list(range(len(testset))),
+            test_size=test_sample_size,
+            random_state=42,
+            stratify=testset.targets,
+        )
         subset = torch.utils.data.Subset(testset, indices)
         data_test = subset.__getitems__(list(range(test_sample_size)))
         X_test = torch.stack(list(map(lambda tup: tup[0], data_test)))
@@ -71,7 +73,7 @@ if __name__ == "__main__":
     norm = Normalize(vmin=0, vmax=1)
     indices = torch.tensor(indices)
 
-    for y,label in enumerate(testset.classes):
+    for y, label in enumerate(testset.classes):
 
         # Set saving directory
         label_dir = os.path.join(base_dir, str(label))
@@ -92,34 +94,53 @@ if __name__ == "__main__":
             # Save image
             image = X[i]
             _, ax = plt.subplots()
-            ax.tick_params(which = "both", bottom = False, top = False, left = False, right = False, labelbottom = False, labelleft = False)
-            ax.imshow(image.numpy().transpose((1,2,0)), cmap="gray", norm=norm)
-            plt.savefig(os.path.join(label_dir, fname))
+            ax.tick_params(
+                which="both",
+                bottom=False,
+                top=False,
+                left=False,
+                right=False,
+                labelbottom=False,
+                labelleft=False,
+            )
+            ax.imshow(image.numpy().transpose((1, 2, 0)), cmap="gray", norm=norm)
+            plt.savefig(os.path.join(label_dir, fname), bbox_inches="tight")
             plt.close()
 
             # Compute and save configs
             V_patches = image.shape[1] // model.patcher.patch_size
             H_patches = image.shape[2] // model.patcher.patch_size
             tot_patches = H_patches * V_patches
-            N_patches = [1, tot_patches // 4, tot_patches // 2, 3*(tot_patches//4), tot_patches]
+            N_patches = [
+                1,
+                tot_patches // 4,
+                tot_patches // 2,
+                3 * (tot_patches // 4),
+                tot_patches,
+            ]
             perc = ["one", "q1", "q2", "q3", "all"]
             x = image.unsqueeze(0)
             if x.dim() == 3:
                 x = x.unsqueeze(0)
 
             transformer_attribution = generate_relevance(model, x).detach()
-            patches_attribution = transformer_attribution.reshape((V_patches, -1, H_patches)).cpu().numpy().mean(axis = 1)
-            sorted_idx = np.argsort(patches_attribution, axis = None)[::-1]
-            sorted_scores = np.sort(patches_attribution, axis = None)[::-1]
+            patches_attribution = (
+                transformer_attribution.reshape((V_patches, -1, H_patches))
+                .cpu()
+                .numpy()
+                .mean(axis=1)
+            )
+            sorted_idx = np.argsort(patches_attribution, axis=None)[::-1]
+            sorted_scores = np.sort(patches_attribution, axis=None)[::-1]
 
-            for n,a in zip(N_patches, perc):
+            for n, a in zip(N_patches, perc):
                 if n == tot_patches:
                     configs[a][fname] = {
                         "objective": 0,
                         "explore": [],
                         "attrib": [],
                         "img_dim": list(image.shape),
-                        "patch_dim": model.patcher.patch_size
+                        "patch_dim": model.patcher.patch_size,
                     }
                 else:
                     configs[a][fname] = {
@@ -127,14 +148,13 @@ if __name__ == "__main__":
                         "explore": sorted_idx[:n].tolist(),
                         "attrib": sorted_scores[:n].tolist(),
                         "img_dim": list(image.shape),
-                        "patch_dim": model.patcher.patch_size
+                        "patch_dim": model.patcher.patch_size,
                     }
 
             del patches_attribution
             del transformer_attribution
             gc.collect()
 
-        for n,config in configs.items():
+        for n, config in configs.items():
             with open(os.path.join(label_dir, f"config_{n}.json"), "w") as f:
                 json.dump(config, f)
-    
