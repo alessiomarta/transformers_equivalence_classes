8b0899b2a7d518323ff6d6a87141846f880760d1
eliroc98 <elisabetta.rocchetti0@gmail.com>
2025-02-12 08:44:52 +0100

    sh for performing image experiements

---
+++ sh-scripts/image-experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ..
+for dir in experiments_data/*; do
+    echo $dir
+    if [[ "$dir" == *"cifar"* || "$dir" == *"mnist"* ]]; then
+        python experiments/vit_exploration.py --experiment-path $dir --out-dir ../res
+        sleep 2
+        for resdir in ../res/*; do
+            if [[ "$resdir" == *"$(basename $dir)"* ]]; then
+                echo $resdir
+                python experiments/vit_exploration_interpretation.py --experiment-path $dir --results-dir $resdir
+            fi
+        done
+    fi
+    
+done
